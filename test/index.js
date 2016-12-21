d5c1d4d36e5101264b8654fc744ef88d000b7f6d
Mark Vayngrib <mark@tradle.io>
2016-12-21 00:14:24 -0500

    more tests

--- test/index.js
+++ test/index.js
@@ -1,18 +1,23 @@
 
+const path = require('path')
 const PassThrough = require('stream').PassThrough
 const { EventEmitter } = require('events')
 const test = require('tape')
 const Promise = require('bluebird')
 const co = Promise.coroutine
+const fs = Promise.promisifyAll(require('fs'))
 const collect = Promise.promisify(require('stream-collector'))
 const tradle = require('@tradle/engine')
 const testHelpers = require('@tradle/engine/test/helpers')
 const changesFeed = require('changes-feed')
 const memdown = require('memdown')
+const parseDataUri = require('parse-data-uri')
+const Onfido = require('@tradle/onfido-api')
 const createOnfidoDB = require('../db')
 const status = require('../status')
 const createOnfido = require('../')
 const fixtures = require('./fixtures')
+const docImages = require('./fixtures/document-images')
 
 // const Onfido = {
 //   applicants:
@@ -184,6 +189,83 @@ test('basic', co(function* (t) {
   t.equal(pending, undefined)
   t.end()
 }))
+
+test.only('integration', co(function* (t) {
+  const rawKeeper = tradle.utils.levelup('./test.db')
+  const keeper = Promise.promisifyAll(rawKeeper)
+  const changes = changesFeed(tradle.utils.levelup('./log.db'))
+  const api = new Onfido({ token: process.env.ONFIDO_API_KEY })
+  const onfido = createOnfido({
+    node: mockNode({ keeper: rawKeeper, changes }),
+    db: 'test',
+    api: api
+  })
+
+  const applicantId = '0cc317bc-00a5-4e4b-8085-4485fceab85a'
+  const photo = parseDataUri(docImages.driving_license)
+  // const doc = yield api.applicants.uploadDocument(applicantId, {
+  //   file: photo.data,
+  //   filename: 'license.' + photo.mimeType.split('/')[1],
+  //   type: 'driving_licence'
+  // })
+
+  const [doc] = require('./fixtures/documents')
+  const check = yield api.checks.createDocumentCheck(applicantId)
+
+  console.log(check)
+  // const applicants = require('./fixtures/applicants.json')
+  // const checks = {}
+  // yield Promise.all(applicants.map(co(function* ({ id }) {
+  //   checks[id] = yield api.checks.list({ applicantId: id, expandReports: true })
+  // })))
+
+  // yield fs.writeFileAsync(path.resolve(__dirname, './fixtures/checks.json'), JSON.stringify(checks, null, 2))
+
+  // yield fs.writeFileAsync(path.resolve(__dirname, './fixtures/applicants.json'), JSON.stringify(applicants, null, 2))
+  t.end()
+}))
+
+// 0cc317bc-00a5-4e4b-8085-4485fceab85a
+
+const createApplicantFixtures = co(function* createApplicantFixtures () {
+  const api = new Onfido({ token: process.env.ONFIDO_API_KEY })
+  const applicants = yield api.applicants.list()
+  yield fs.writeFileAsync(path.resolve(__dirname, './fixtures/applicants.json'), JSON.stringify(applicants, null, 2))
+})
+
+const createDocumentFixtures = co(function* createDocumentFixtures () {
+  const api = new Onfido({ token: process.env.ONFIDO_API_KEY })
+  const applicants = require('./fixtures/applicants')
+  const documents = {}
+  yield Promise.all(applicants.map(co(function* ({ id }) {
+    documents[id] = yield api.applicants.listDocuments(applicantId)
+  })))
+
+  yield fs.writeFileAsync(path.resolve(__dirname, './fixtures/documents.json'), JSON.stringify(documents, null, 2))
+})
+
+const createCheckFixtures = co(function* createCheckFixtures () {
+  const api = new Onfido({ token: process.env.ONFIDO_API_KEY })
+  const applicants = require('./fixtures/applicants')
+  const checks = {}
+  yield Promise.all(applicants.map(co(function* ({ id }) {
+    checks[id] = yield api.checks.list({ applicantId: id, expandReports: true })
+  })))
+
+  yield fs.writeFileAsync(path.resolve(__dirname, './fixtures/checks.json'), JSON.stringify(checks, null, 2))
+})
+
+const createWebhookFixtures = co(function* createWebhookFixtures () {
+  const webhooks = api.webhooks.list()
+  yield fs.writeFileAsync(path.resolve(__dirname, './fixtures/webhooks.json'), JSON.stringify(webhooks, null, 2))
+})
+
+const createFixtures = co(function* createFixtures () {
+  yield createApplicantFixtures()
+  yield createDocumentFixtures()
+  yield createCheckFixtures()
+  yield createWebhookFixtures()
+})
 
 function mockNode ({ keeper, changes }) {
   const ee = new EventEmitter()
